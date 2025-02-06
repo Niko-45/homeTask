0fe72cd92194382202d481b56c1ee66d05b9eded
Alisher <roziqovalisher177@gmail.com>
2025-02-06 19:16:20 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,342 @@
+import "./App.css";
+export default function App() {
+  return (
+    <div>
+      <div className="nav">
+        <img src="/src/img/Logo.png" alt="" />
+        <input type="text" placeholder="search" />
+        <select name="" id="">
+          <option value="">Category</option>
+        </select>
+        <p>Become a mentor</p>
+        <button className="btn">Log in</button>
+        <button className="buttons">Sign up</button>
+      </div>
+      <div className="section1">
+        <div className="left1">
+          <h1>Find the Best Courses and Amazing Mentor</h1>
+          <h2>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+            purus sit amet luctus venenatis, lectus magna fringilla urna,
+            porttitor rhoncus{" "}
+          </h2>
+          <div className="inpDiv">
+            <input type="text" placeholder="search" />
+            <button>Search</button>
+          </div>
+          <div className="titles">
+            <p>Life Time Access</p>
+            <p>Expert Mentor</p>
+            <p>100K+ Courses</p>
+          </div>
+        </div>
+        <div className="right1">
+          <img src="/src/img/Image Place Holder.png" className="img1" alt="" />
+        </div>
+      </div>
+      <div className="section2">
+        <h1>Trending Courses</h1>
+        <div>
+          <button  className="buttons">All</button>
+          <button>Business</button>
+          <button>Art & Design</button>
+          <button>Development</button>
+          <button>Marketing</button>
+        </div>
+        <div className="grid1">
+          <div className='divGr1' >
+            <img src="/src/img/Image Placeholder.png" alt="" />
+            <h4>Interpersonal skills - work better with others!</h4>
+            <p>Tanah Air Team</p>
+            <img src="/src/img/Rating.png" alt="" />
+            <div className="pDiv">
+              <span>$640</span>
+              <p>
+                <b>$384</b>
+              </p>
+            </div>
+          </div>
+          <div className='divGr1'>
+            <img src="/src/img/Image Placeholder.png" alt="" />
+            <h4>Interpersonal skills - work better with others!</h4>
+            <p>Tanah Air Team</p>
+            <img src="/src/img/Rating.png" alt="" />
+            <div className="pDiv">
+              <span>$640</span>
+              <p>
+                <b>$384</b>
+              </p>
+            </div>
+          </div>
+          <div className='divGr1'>
+            <img src="/src/img/Image Placeholder.png" alt="" />
+            <h4>Interpersonal skills - work better with others!</h4>
+            <p>Tanah Air Team</p>
+            <img src="/src/img/Rating.png" alt="" />
+            <div className="pDiv">
+              <span>$640</span>
+              <p>
+                <b>$384</b>
+              </p>
+            </div>
+          </div>
+          <div className='divGr1'>
+            <img src="/src/img/Image Placeholder.png" alt="" />
+            <h4>Interpersonal skills - work better with others!</h4>
+            <p>Tanah Air Team</p>
+            <img src="/src/img/Rating.png" alt="" />
+            <div className="pDiv">
+              <span>$640</span>
+              <p>
+                <b>$384</b>
+              </p>
+            </div>
+          </div>
+          <div className='divGr1'>
+            <img src="/src/img/Image Placeholder.png" alt="" />
+            <h4>Interpersonal skills - work better with others!</h4>
+            <p>Tanah Air Team</p>
+            <img src="/src/img/Rating.png" alt="" />
+            <div className="pDiv">
+              <span>$640</span>
+              <p>
+                <b>$384</b>
+              </p>
+            </div>
+          </div>
+          <div className='divGr1'>
+            <img src="/src/img/Image Placeholder.png" alt="" />
+            <h4>Interpersonal skills - work better with others!</h4>
+            <p>Tanah Air Team</p>
+            <img src="/src/img/Rating.png" alt="" />
+            <div className="pDiv">
+              <span>$640</span>
+              <p>
+                <b>$384</b>
+              </p>
+            </div>
+          </div>
+          <div className='divGr1'>
+            <img src="/src/img/Image Placeholder.png" alt="" />
+            <h4>Interpersonal skills - work better with others!</h4>
+            <p>Tanah Air Team</p>
+            <img src="/src/img/Rating.png" alt="" />
+            <div className="pDiv">
+              <span>$640</span>
+              <p>
+                <b>$384</b>
+              </p>
+            </div>
+          </div>
+          <div className='divGr1'>
+            <img src="/src/img/Image Placeholder.png" alt="" />
+            <h4>Interpersonal skills - work better with others!</h4>
+            <p>Tanah Air Team</p>
+            <img src="/src/img/Rating.png" alt="" />
+            <div className="pDiv">
+              <span>$640</span>
+              <p>
+                <b>$384</b>
+              </p>
+            </div>
+          </div>
+        </div>
+        <button className="buttons">More Courses</button>
+      </div>
+      <div className="Categories1">
+        <h1>Top Categories</h1>
+        <div className="Categories">
+          <div>
+            <img src="/src/img/Overlay.png" alt="" />
+            <p>Art & Design</p>
+          </div>
+          <div>
+            <img src="/src/img/Overlay.png" alt="" />
+            <p>Art & Design</p>
+          </div>
+          <div>
+            <img src="/src/img/Overlay.png" alt="" />
+            <p>Art & Design</p>
+          </div>
+          <div>
+            <img src="/src/img/Overlay.png" alt="" />
+            <p>Art & Design</p>
+          </div>
+          <div>
+            <img src="/src/img/Overlay.png" alt="" />
+            <p>Art & Design</p>
+          </div>
+          <div>
+            <img src="/src/img/Overlay.png" alt="" />
+            <p>Art & Design</p>
+          </div>
+          <div>
+            <img src="/src/img/Overlay.png" alt="" />
+            <p>Art & Design</p>
+          </div>
+          <div>
+            <img src="/src/img/Overlay.png" alt="" />
+            <p>Art & Design</p>
+          </div>
+        </div>
+      </div>
+      <div className="lib">
+        <h1>Top Categories</h1>
+        <div className="libAll">
+          <div className="Lib">
+            <img src="/src/img/video_library.png" alt="" />
+            <p>
+              <b>10.000+</b>
+            </p>
+            <p>Courses</p>
+          </div>
+          <div className="Lib">
+            <img src="/src/img/video_library.png" alt="" />
+            <p>
+              <b>50.000+</b>
+            </p>
+            <p>Active Students</p>
+          </div>
+          <div className="Lib">
+            <img src="/src/img/video_library.png" alt="" />
+            <p>
+              <b>1000+</b>
+            </p>
+            <p>Active Mentor</p>
+          </div>
+          <div className="Lib">
+            <img src="/src/img/video_library.png" alt="" />
+            <p>
+              <b>180</b>
+            </p>
+            <p>Countries</p>
+          </div>
+        </div>
+      </div>
+      <div className="mentor">
+        <div>
+          <img src="/src/img/Image Placeholder (2).png" alt="" />
+        </div>
+        <div className="div2">
+          <h1>Become an Mentor</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat eu
+            in orci, nunc amet libero. Nam scelerisque vestibulum bibendum a
+            turpis. Ante feugiat lectus massa, odio amet. Auctor sit mattis non
+            id proin elit placerat. Lectus morbi amet et aliquam magna mauris.
+          </p>
+          <br />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat eu
+            in orci, nunc amet libero. Nam scelerisque vestibulum bibendum a
+            turpis. Ante feugiat lectus massa, odio amet. Auctor sit mattis non
+            id proin elit placerat. Lectus morbi amet et aliquam magna mauris.
+          </p>
+          <button className="buttons">Start Teaching</button>
+        </div>
+      </div>
+      <div className="about">
+        <div>
+          <img src="/src/img/Image Placeholder.png" alt="" />
+        </div>
+        <div className="ab1">
+          <h3>
+            After studying here I gained new knowledge and insights. The mentor
+            was very friendly and guided me to get the knowledge I wanted. Thank
+            you!
+          </h3>
+          <p>KecekArif</p>
+          <p>Yang punya Surabaya nanti kapan - kapan tapi</p>
+        </div>
+      </div>
+      <div className="company1">
+        <h1>Trusted by Company</h1>
+        <div className="company">
+          <img src="/src/img/01.png" alt="" />
+          <img src="/src/img/02.png" alt="" />
+          <img src="/src/img/05.png" alt="" />
+          <img src="/src/img/02.png" alt="" />
+          <img src="/src/img/05.png" alt="" />
+          <img src="/src/img/01.png" alt="" />
+        </div>
+      </div>
+      <div className="blog">
+        <h1>The Latest Blog</h1>
+        <div className="blog1">
+          <img src="" alt="" />
+          <p>3, Mei 2021 3 min read</p>
+          <h4>10 graphic design trends to get you inspired in 2021</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do
+            eiusmod.
+          </p>
+          <p>Read More</p>
+        </div>
+        <div className="blog1">
+          <img src="" alt="" />
+          <p>3, Mei 2021 3 min read</p>
+          <h4>10 graphic design trends to get you inspired in 2021</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do
+            eiusmod.
+          </p>
+          <p>Read More</p>
+        </div>
+        <div className="blog1">
+          <img src="" alt="" />
+          <p>3, Mei 2021 3 min read</p>
+          <h4>10 graphic design trends to get you inspired in 2021</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consec tetur adipiscing elit, sed do
+            eiusmod.
+          </p>
+          <p>Read More</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet
+              tristique placerat eleifend aliquam pellentesque facilisis
+              ultrices. Imperdiet mus urna egestas at tellus.{" "}
+            </p>
+          </div>
+          <div>
+            <p>
+              <b>About</b>
+            </p>
+            <p>About us</p>
+            <p>Courses</p>
+            <p>Mentor</p>
+            <p>Contact Us</p>
+          </div>
+          <div>
+            <p>
+              <b>Support</b>
+            </p>
+            <p>Careers</p>
+            <p>Help & Support</p>
+            <p>Blog</p>
+          </div>
+          <div>
+            <p>
+              <b>Company</b>
+            </p>
+            <p>Privacy Policy</p>
+            <p>Terms of Service</p>
+            <p>Code of Conduct</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>Privacy Policy</p>
+            <p>Terms and Condition</p>
+          </div>
+          <p>@2020 TanahAir Studio. All rights reserved.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
